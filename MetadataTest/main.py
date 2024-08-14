839d02e21be97ab76a0fc62b5f1cf38ae7bb0d5c
m-shao <dev.minglunshao@gmail.com>
2024-08-13 21:59:19 -0400

    feat/turn metadata editing in function with timing stress test

--- MetadataTest/main.py
+++ MetadataTest/main.py
@@ -1,26 +1,37 @@
 import exiftool
+import time
 
-# Define an array of file paths
-base_path = r"C:\Users\drago\Desktop\Python Projects\photo-organization-tool\MetadataTest"
-file_paths = ["edit1.JPG", "edit2.JPG", "edit3.JPG", "YOMAMA.txt", "doesnotexist.JPG"]
+def set_metadata(base_path, file_path, subject, new_value):
+    with exiftool.ExifTool() as et:
+        result = et.execute(f"-{subject}={new_value}".encode('utf-8'), b"-overwrite_original", (base_path + "\\" + file_path).encode('utf-8'))
+    return result
 
-# Define the new title
-new_rating = "5"
 
-# Prepare the arguments for ExifTool
-args = [b"-Rating=" + new_rating.encode('utf-8'), b"-overwrite_original"]
+if __name__ == "__main__":
+    # Define an array of file paths
+    base_path = r"C:\Users\drago\Desktop\Python Projects\photo-organization-tool\MetadataTest"
+    # file_paths = ["edit1.JPG", "edit2.JPG", "edit3.JPG"]
+    file_paths = [f"edit1 - Copy ({i + 2})" for i in range(35)]
 
-# Add the file paths to the argument list
-args.extend([(base_path + "\\" + file_path).encode('utf-8') for file_path in file_paths])
+    # Define the new title
+    subject = "Rating"
+    new_rating = "5"
 
-failed_files = []
+    start_time = time.time()
 
-# Use ExifTool to set the title and overwrite the original files
-with exiftool.ExifTool() as et:
-    result = et.execute(*args)
+    for file in file_paths:
+        set_metadata(base_path, file, subject, new_rating)
+        print(file)
 
-# Check the result to determine if any files failed
-print(result.splitlines())
+    # Stop the timer
+    end_time = time.time()
+
+    # Calculate the time taken
+    time_taken = end_time - start_time
+
+    # Print the time taken
+    print(f"\nTime taken : {time_taken:.4f} seconds")
+
 # if result:
 #     output = result.decode('utf-8').splitlines()
 #     for line in output:
