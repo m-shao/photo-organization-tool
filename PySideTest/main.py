f11d72e1326c8196b811e609bfddc438009b106a
m-shao <dev.minglunshao@gmail.com>
2024-08-13 23:23:35 -0400

    fix/upgrade python version and rewrite venv

--- PySideTest/main.py
+++ PySideTest/main.py
@@ -111,16 +111,16 @@
 
 # Version 1: QMainWindow
 
-# from PySide6.QtWidgets import QApplication
-# from main_window import MainWindow
-#
-# import sys
-#
-# app = QApplication(sys.argv)
-#
-# window = MainWindow(app)
-# window.show()
-#
-# app.exec()
+from PySide6.QtWidgets import QApplication
+from main_window import MainWindow
+
+import sys
+
+app = QApplication(sys.argv)
+
+window = MainWindow(app)
+window.show()
+
+app.exec()
 
 # Version 1: QMessageBox
